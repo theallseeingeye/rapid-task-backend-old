410bb77c6536f7232f384be0cbba1058224b982c
theallseeingeye <sikstrom@rapidtask.com>
2018-03-02 19:40:07 -0700

    Updated the requirements folder

--- rapidtaskbackend/settings/base.py
+++ rapidtaskbackend/settings/base.py
@@ -29,7 +29,7 @@ import os
 #         raise ImproperlyConfigured(error_msg)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret('SECRET_KEY')
+# SECRET_KEY = get_secret('SECRET_KEY')
 
 
 
