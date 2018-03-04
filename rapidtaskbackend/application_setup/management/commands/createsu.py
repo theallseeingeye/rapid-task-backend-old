2c99becf6aba27689ef8e2502be2e177cd50d1cd
theallseeingeye <sikstrom@rapidtask.com>
2018-03-03 18:03:39 -0700

    Tinkering with settings to be launched on aws

--- rapidtaskbackend/application_setup/management/commands/createsu.py
+++ rapidtaskbackend/application_setup/management/commands/createsu.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.core.exceptions import ImproperlyConfigured
-import json
+# import json
+import os
 
 
 # This sets up the user for the admin on the initial build of the api. AWS doesn't have
@@ -9,25 +10,25 @@ import json
 # us to tell AWS to create a superuser on build.
 
 # JSON-based secrets module. This is where it looks for the secret key file and reads it.
-with open('secrets.json') as f:
-    secrets = json.loads(f.read())
-
-
-def get_secret(setting, secrets=secrets):
-    # Get the secret variable or return explicit exception.
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = 'Set the {0} environment variable'.format(setting)
-        raise ImproperlyConfigured(error_msg)
-
+# with open('secrets.json') as f:
+#     secrets = json.loads(f.read())
+#
+#
+# def get_secret(setting, secrets=secrets):
+#     # Get the secret variable or return explicit exception.
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         error_msg = 'Set the {0} environment variable'.format(setting)
+#         raise ImproperlyConfigured(error_msg)
+#
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username=get_secret("SUPERUSERNAME")).exists():
+        if not User.objects.filter(username=os.environ("SUPERUSERNAME")).exists():
             User.objects.create_superuser(
-                get_secret('SUPERUSERNAME'),
-                get_secret('SUPERUSEREMAIL'),
-                get_secret('SUPERUSERPASSWORD')
+                os.environ('SUPERUSERNAME'),
+                os.environ('SUPERUSEREMAIL'),
+                os.environ('SUPERUSERPASSWORD')
             )
