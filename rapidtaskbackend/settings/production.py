ea335f56a6c8ba259e7811def24a40892e912873
theallseeingeye <sikstrom@rapidtask.com>
2018-03-03 21:06:31 -0700

    Added web security features and email linked

--- rapidtaskbackend/settings/production.py
+++ rapidtaskbackend/settings/production.py
@@ -6,7 +6,6 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'backend-init-deploy-test.us-west-2.elasticbeanstalk.com',
     '.rapidtask.com',
-    'localhost'
     ]
 
 
@@ -25,11 +24,11 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 
 # Forces the use of cookies over HTTPS
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # This prevents access to the stored data from JavaScript
-# SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_HTTPONLY = True
 
 # Django honeypot admin page settings
 # This will send any logged attempts to the /admin page to all the administrator's emails.
@@ -38,12 +37,12 @@ ADMIN_HONEYPOT_EMAIL_ADMIN = True
 # Django email settings
 # Note: This will need to be changed for production setting using a different email service.
 # Google is not a transactional email service and not made for web applications.
-# EMAIL_BACKEND = 'django.cor.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'sikstrom@rapidtask.com'
-# EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.cor.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sikstrom@rapidtask.com'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
+EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = True
 # EMAIL_SSL_CERTFILE = None
 # EMAIL_SSL_KEYFILE = None
@@ -51,18 +50,18 @@ ADMIN_HONEYPOT_EMAIL_ADMIN = True
 
 # HTTP Strict Transport Security - Forces browsers to use HTTPS
 # The time is telling the browser how long to remember the forced redirect to HTTPS
-# SECURE_HSTS_SECONDS = 3600 # This is 1 hour- change to one year after testing is done. Google wants to see 63072000 after all the testings are done.
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 3600 # This is 1 hour- change to one year after testing is done. Google wants to see 63072000 after all the testings are done.
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
-# SECURE_CONTENT_TYPE_NOSNIFF = True # Ensures that browsers to identify content types correctly.
-# SECURE_BROWSER_XSS_FILTER = True # Helps prevents XSS attacks
-# SECURE_SSL_REDIRECT = True
+SECURE_CONTENT_TYPE_NOSNIFF = True # Ensures that browsers to identify content types correctly.
+SECURE_BROWSER_XSS_FILTER = True # Helps prevents XSS attacks
+SECURE_SSL_REDIRECT = True
 
-# X_FRAME_OPTIONS = 'DENY' # Change to SAMEORIGIN if we choose to use iframes with google maps.
+X_FRAME_OPTIONS = 'DENY' # Change to SAMEORIGIN if we choose to use iframes with google maps.
 #
-# CORS_ORIGIN_WHITELIST = (
-#     'backend-init-deploy-test.us-west-2.elasticbeanstalk.com',
-#     'www.rapidtask.com',
-#     'rapidtask.com',
-# )
+CORS_ORIGIN_WHITELIST = (
+    'backend-init-deploy-test.us-west-2.elasticbeanstalk.com',
+    'www.rapidtask.com',
+    'rapidtask.com',
+)
